c4fec09b54e55cfc48e10cfcf74bce9e3d34d5d9
camilacorrea91 <camilacorreaa91@gmail.com>
2022-06-09 15:41:13 -0300

    botao screenshot, enviar comentário, janela de sucesso do feedback

--- src/components/widgetForm/index.tsx
+++ src/components/widgetForm/index.tsx
@@ -5,6 +5,7 @@ import otherImageUrl from '../../assets/thought.svg';
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypesStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -42,16 +43,30 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
 
-const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback(){
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
 
   return(
     <div className="bg-zinc-900 p-4 position-relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-
-       {!feedbackType ? (
-          <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
-        ) : (
-            <FeedbackContentStep feedbackType={feedbackType} />
-        )}
+      { feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+          ) : (
+            <FeedbackContentStep feedbackType={feedbackType}
+            onFeedbackRestartRequested={handleRestartFeedback}
+            onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      ) }
 
         <footer className="text-xs text-neutral-400">
           Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat </a>
